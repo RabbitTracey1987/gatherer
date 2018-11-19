29ba395dde384fdd591c883bb0a48afe40e072ae
Ji Wang <traceyw@vt.edu>
2018-11-19 15:40:36 -0500

    add some rspec

---
+++ app/models/project.rb
@@ -0,0 +1,19 @@
+class Project
+	attr_accessor :tasks
+
+	def initialize
+		@tasks = []
+	end
+	def incomplete_tasks
+		tasks.reject(&:complete?)
+	end
+	def done?
+		tasks.reject(&:complete?).empty?
+	end
+	def total_size
+		tasks.sum(&:size)
+	end
+	def remaining_size
+		incomplete_tasks.sum(&:size)
+	end
+end
